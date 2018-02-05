375f002920d001fa013900866c9bc1d087f4d9ba
Andlat <andlat@andlat.local>
2018-02-05 07:59:43 -0500

    Ajout de tests
    Fichiers tests pour le game engine

--- Day_n_Night/app/src/main/java/daynight/daynnight/MainActivity.java
+++ Day_n_Night/app/src/main/java/daynight/daynnight/MainActivity.java
@@ -1,13 +1,32 @@
 package daynight.daynnight;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import daynight.daynnight.engine.GLManager;
+
 public class MainActivity extends AppCompatActivity {
+    private GLSurfaceView mGLSurface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mGLSurface = new GLManager.GLSurface(this);
+
+        setContentView(mGLSurface);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mGLSurface.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mGLSurface.onResume();
     }
 }
