2c53b39cc7c2b6d49954e0ea6748fe2a82b16607
mascool <antoine.mascolo3428@gmail.com>
2018-02-21 16:53:54 -0500

    Maquette pause/menu

--- Day_n_Night/app/src/main/java/daynight/daynnight/MainActivity.java
+++ Day_n_Night/app/src/main/java/daynight/daynnight/MainActivity.java
@@ -1,8 +1,13 @@
 package daynight.daynnight;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,12 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Test
 
-        Button buttonMap = (Button) findViewById(R.id.jourButton);
-        buttonMap.setOnClickListener(new View.OnClickListener() {
+        Button buttonDay = (Button) findViewById(R.id.jourButton);
+        buttonDay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button buttonNight = (Button) findViewById(R.id.nuitButton);
+        buttonNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Ceci est juste un test pour le bouton pause
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                builder.setView(inflater.inflate(R.layout.pause_menu, null));
+
+                builder.show();
             }
         });
     }
