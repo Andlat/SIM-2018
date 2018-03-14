a1be81c76e962766353445b9448a4d0e1a277bbf
mascool <antoine.mascolo3428@gmail.com>
2018-03-14 13:42:08 -0400

    interruption1

--- Day_n_Night/app/src/main/java/daynight/daynnight/MapActivity.java
+++ Day_n_Night/app/src/main/java/daynight/daynnight/MapActivity.java
@@ -59,6 +59,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     private LocationManager locationManager;
     private LatLng livePos;
     private LatLng prevPos;
+    private LatLng poiUpdate;
+    private float[] distanceFromPoiUpdate;
     private float[] move;
     private LocationListener locationListener;
     private Button boutonCenter;
@@ -75,9 +77,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-
-        move = new float[1];
-        prevPos = new LatLng(0, 0);
 
         //Si la permission de localisation n'est pas donné une fenêtre la demande
         if (ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -155,6 +154,11 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
             }
         });
 
+        move = new float[1];
+        distanceFromPoiUpdate = new float[1];
+        prevPos = new LatLng(0, 0);
+        poiUpdate = new LatLng(0,0);
+
         //Stylisation de la carte avec JSON d'un Raw.xml
         try {
 
@@ -172,6 +176,12 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
                 public void onLocationChanged(Location location) {
 
                     livePos = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    //Va chercher les coordonnés des poi dans un rayon de 50km
+                    Location.distanceBetween(poiUpdate.latitude, poiUpdate.longitude, livePos.latitude, livePos.longitude, distanceFromPoiUpdate);
+                    if(distanceFromPoiUpdate[0] > 20000){
+
+                    }
 
                     Log.d("POS", livePos.toString());
                     //Distance entre la position actuelle et la dernière actualisation
