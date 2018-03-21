b5b12acbd87a0ea25ac2d84f1441065d8d8ee9b8
anthonymartin <anthom_13@hotmail.com>
2018-03-21 12:32:26 -0400

    ajout de boutons drawable

--- Day_n_Night/app/src/main/java/daynight/daynnight/GameActivity.java
+++ Day_n_Night/app/src/main/java/daynight/daynnight/GameActivity.java
@@ -2,12 +2,44 @@ package daynight.daynnight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
-
+    Button buttonRecommencer;
+    Button buttonMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        buttonRecommencer = (Button)findViewById(R.id.buttonRecommencer);
+        //Changement couleur bouton lorsque touché
+        buttonRecommencer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN ) {
+                    buttonRecommencer.setBackground(getDrawable(R.drawable.bouton4_2));
+                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                    buttonRecommencer.setBackground(getDrawable(R.drawable.bouton4_1));
+                }
+                return false;
+            }
+        });
+
+        buttonMenu = (Button)findViewById(R.id.buttonMenu);
+        //Changement couleur bouton lorsque touché
+        buttonMenu.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN ) {
+                    buttonMenu.setBackground(getDrawable(R.drawable.bouton4_2));
+                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                    buttonMenu.setBackground(getDrawable(R.drawable.bouton4_1));
+                }
+                return false;
+            }
+        });
     }
 }
