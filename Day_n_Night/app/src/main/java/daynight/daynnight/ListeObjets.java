6288b4ede2f8926caf83986ac7d8077b00ac9700
Sébastien St-Denis <sebastienst-denis@hotmail.com>
2018-03-21 13:56:03 -0400

    Boutons

--- Day_n_Night/app/src/main/java/daynight/daynnight/ListeObjets.java
+++ Day_n_Night/app/src/main/java/daynight/daynnight/ListeObjets.java
@@ -66,7 +66,7 @@ public class ListeObjets extends Fragment
                 //extras.putParcelable("objet", objets.get(position));
                 //intent.putExtras(extras);
                 //startActivity(intent);
-                infosObjetInventaire.startActivity(objets.get(position), getContext(), true);
+                infosObjetInventaire.startActivity(objets.get(position), getContext(), false);
             }
         });
 
